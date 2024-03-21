c27dc24383c67efce6883630d32ebec093113010
Beesigaa <adejohadamah@gmail.com>
2024-03-21 14:06:40 -0700

    working on base_model

---
+++ models/base_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+"""
+import uuid
+import datetime
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+
+    def save(self):
+        """
+           
+        """
+        self.updated_at = datetime.utcnow()
+
+    def to_dict(self):
+        """
+        """
+        inst_dict = self.__dict__.copy()
+        inst_dict["__class__"] = self.__class__.__name__
+        inst_dict["created_at"] = self.created_at.isoformat()
+        inst_dict["updated_at"] = self.updated_at.isoformat()
+
+        return inst_dict
+
+    def __str__:
+        """
+        
+        """
+        class_name = self.__class__.name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
